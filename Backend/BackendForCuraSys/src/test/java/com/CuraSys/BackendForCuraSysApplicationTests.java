574e315697635a82986ab61c19c6fbc4086000d6
Vaibhav Chavan <vaibhavchavan034@gmail.com>
2025-08-06 15:51:52 +0530

    backend template created

---
+++ Backend/BackendForCuraSys/src/test/java/com/CuraSys/BackendForCuraSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CuraSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendForCuraSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

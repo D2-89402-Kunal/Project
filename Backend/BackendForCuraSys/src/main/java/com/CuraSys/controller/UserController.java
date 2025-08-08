7859d39aeceb19cbaa4e8b5518142fb739199e29
Mallinath Bharamshetti <mallinathbharamshettinv@gmail.com>
2025-08-08 16:29:28 +0530

    added a method signup

--- Backend/BackendForCuraSys/src/main/java/com/CuraSys/controller/UserController.java
+++ Backend/BackendForCuraSys/src/main/java/com/CuraSys/controller/UserController.java
@@ -1,10 +1,18 @@
 package com.CuraSys.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.CuraSys.entities.User;
 import com.CuraSys.repository.UserRepository;
 import com.CuraSys.service.UserService;
 
@@ -18,8 +26,21 @@ public class UserController {
 	@Autowired
 	public UserRepository userRepository;
 	
+//	@Autowired
+//	private PasswordEncoder passwordEncoder; 
+
+	
 	@GetMapping("/patients/count")
 	public long getPatientCount() {
 		return userService.getPatientCounts();
+	}
+	
+	@PostMapping("/signup")
+	public ResponseEntity<?> userSignup(@Validated @RequestBody User u){
+		System.out.print("in signup");
+		return ResponseEntity
+				.status(HttpStatus.CREATED)
+				.body(userService.userRegistration(u));
+				
 	}
 }
--- Backend/BackendForCuraSys/src/main/java/com/CuraSys/service/UserServiceImpl.java
+++ Backend/BackendForCuraSys/src/main/java/com/CuraSys/service/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.CuraSys.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.CuraSys.entities.User;
 import com.CuraSys.repository.UserRepository;
 
 @Service
@@ -13,9 +15,19 @@ public class  UserServiceImpl implements UserService {
 	@Autowired
 	public UserRepository userRepository;
 
+//	@Autowired
+//	private PasswordEncoder encoder;
+	
 	@Override
 	public long getPatientCounts() {
 		return userRepository.getPatientCount();
+	}
+
+	@Override
+	public User userRegistration(User u) {
+		u.setPhoneNumber("+91"+u.getPhoneNumber());
+		//u.setPassword(encoder.encode(u.getPassword()));
+		return userRepository.save(u);
 	}
 	
 	
